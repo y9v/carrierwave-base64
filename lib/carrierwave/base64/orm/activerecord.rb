3c81e4748a5ba629fd275043d92aae833e9801fd
Víctor Viruete <victor@diacode.com>
2015-02-17 18:26:42 +0100

    Ensuring the attachment is a string to prevent calling strip method on a `ActionDispatch::Http::UploadedFile` object

--- lib/carrierwave/base64/orm/activerecord.rb
+++ lib/carrierwave/base64/orm/activerecord.rb
@@ -7,7 +7,7 @@ module Carrierwave
         mount_uploader attribute, uploader_class
 
         define_method "#{attribute}=" do |data|
-          if data.present? && data.strip.end_with?("==")
+          if data.present? && data.is_a?(String) && data.strip.end_with?("==")
             super(Carrierwave::Base64::Base64StringIO.new(data.strip))
           else
             super(data)
