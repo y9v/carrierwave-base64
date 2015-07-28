4f1e973b056731c3de519d6c303ea2093a83cd1e
Vadim Poplavskiy <im@demetrodon.com>
2015-07-28 16:48:41 +0300

    raise ArgumentError if nsstring returns nil and base64 data eql "(null)"

--- lib/carrierwave/base64/base64_string_io.rb
+++ lib/carrierwave/base64/base64_string_io.rb
@@ -9,6 +9,7 @@ module Carrierwave
         description, encoded_bytes = encoded_file.split(",")
 
         raise ArgumentError unless encoded_bytes
+        raise ArgumentError if encoded_bytes.eql?("(null)")
 
         @file_format = get_file_format description
         bytes = ::Base64.decode64 encoded_bytes
