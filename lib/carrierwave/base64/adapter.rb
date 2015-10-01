b14cf8f0e7eebd828a2219dc46ec0847d896f71e
Ben Woodward <b@benw.me>
2015-10-01 19:19:12 +1000

    add mount_base64_uploaders method

--- lib/carrierwave/base64/adapter.rb
+++ lib/carrierwave/base64/adapter.rb
@@ -12,6 +12,22 @@ module Carrierwave
           end
         end
       end
+
+      def mount_base64_uploaders(attribute, uploader_class, options = {})
+        mount_uploaders attribute, uploader_class, options
+
+        define_method "#{attribute}=" do |data|
+          if data.present? && data.is_a?(Array) && data.all? { |d| d.is_a?(String) } && data.all? { |d| d.strip.start_with?("data") }
+            files = []
+            data.each do |d|
+              files << Carrierwave::Base64::Base64StringIO.new(d.strip)
+            end
+            super(files)
+          else
+            super([data])
+          end
+        end
+      end
     end
   end
 end
