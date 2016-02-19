89c26880f2a4a6c5a7caf11787336d65de50c2e0
Nicolas Oga <2112.oga@gmail.com>
2016-02-19 17:26:55 -0300

    Fix bug where I can't send regular files to attribute writter

--- lib/carrierwave/base64/adapter.rb
+++ lib/carrierwave/base64/adapter.rb
@@ -5,27 +5,23 @@ module Carrierwave
         mount_uploader attribute, uploader_class, options
 
         define_method "#{attribute}=" do |data|
-          if data.present? && data.is_a?(String) && data.strip.start_with?("data")
-            super(Carrierwave::Base64::Base64StringIO.new(data.strip))
-          else
-            super(data)
-          end
+          super Carrierwave::Base64::Adapter.handle_data(data)
         end
       end
 
       def mount_base64_uploaders(attribute, uploader_class, options = {})
         mount_uploaders attribute, uploader_class, options
 
-        define_method "#{attribute}=" do |data|
-          if data.present? && data.is_a?(Array) && data.all? { |d| d.is_a?(String) } && data.all? { |d| d.strip.start_with?("data") }
-            files = []
-            data.each do |d|
-              files << Carrierwave::Base64::Base64StringIO.new(d.strip)
-            end
-            super(files)
-          else
-            super([data])
-          end
+        define_method "#{attribute}=" do |data_ary|
+          super data_ary.map(&Carrierwave::Base64::Adapter.method(:handle_data))
+        end
+      end
+
+      def self.handle_data(data)
+        if data.present? && data.is_a?(String) && data.strip.start_with?("data")
+          Carrierwave::Base64::Base64StringIO.new(data.strip)
+        else
+          data
         end
       end
     end
