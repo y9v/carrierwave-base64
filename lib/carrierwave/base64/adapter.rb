ceb9b4ebac31694a15c3ebc8f9b2d9bd9da0f5e2
Tsung-en Hsiao <joshua5201.eecs03@nctu.edu.tw>
2016-05-08 02:09:09 +0800

    Add JSON string support in mount_base64_uploaders

--- lib/carrierwave/base64/adapter.rb
+++ lib/carrierwave/base64/adapter.rb
@@ -6,6 +6,7 @@ module Carrierwave
 
         define_method "#{attribute}=" do |data|
           if data.present? && data.is_a?(String) && data.strip.start_with?("data")
+            send "#{attribute}_will_change!" if data.present?
             super(Carrierwave::Base64::Base64StringIO.new(data.strip))
           else
             super(data)
@@ -17,12 +18,18 @@ module Carrierwave
         mount_uploaders attribute, uploader_class, options
 
         define_method "#{attribute}=" do |data|
-          if data.present? && data.is_a?(Array) && data.all? { |d| d.is_a?(String) } && data.all? { |d| d.strip.start_with?("data") }
-            files = []
-            data.each do |d|
-              files << Carrierwave::Base64::Base64StringIO.new(d.strip)
-            end
-            super(files)
+          send "#{attribute}_will_change!" if data.present?
+
+          if data.is_a?(Array)
+            base64_data = data
+          elsif data.is_a?(String)
+            base64_data = JSON.parse(data)
+          else
+            base64_data = []
+          end
+
+          if data.present? && base64_data.all? {|d| d.is_a?(String) and d.strip.start_with?("data")}
+            super(base64_data.map{|d| Carrierwave::Base64::Base64StringIO.new(d.strip)})
           else
             super([data])
           end
