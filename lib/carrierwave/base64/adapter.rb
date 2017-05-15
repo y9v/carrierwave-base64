e4edddbca062b0656b1c40f4b1e2995f19950e0d
Jordan Sitkin <jordan@fiftyfootfoghorn.com>
2017-05-15 10:45:03 -0700

    move file_name default out of attribute= method, support Strings
    A further fix for the file_name as Proc feature. This _actually_ prevents the mutation of `options[:file_name]` as originally intended, and does not remove the support for setting `options[:file_name]` to a String.

--- lib/carrierwave/base64/adapter.rb
+++ lib/carrierwave/base64/adapter.rb
@@ -6,7 +6,8 @@ module Carrierwave
       # rubocop:disable Metrics/PerceivedComplexity
       def mount_base64_uploader(attribute, uploader_class, options = {})
         mount_uploader attribute, uploader_class, options
-
+        options[:file_name] ||= -> { attribute }
+        
         define_method "#{attribute}=" do |data|
           return if data == send(attribute).to_s
 
@@ -17,10 +18,13 @@ module Carrierwave
           return super(data) unless data.is_a?(String) &&
                                     data.strip.start_with?('data')
 
-          options[:file_name] ||= -> { attribute }
-          super(Carrierwave::Base64::Base64StringIO.new(
-            data.strip, options[:file_name].call(self)
-          ))
+          filename = if options[:file_name].respond_to?(:call)
+                      options[:file_name].call(self)
+                    else
+                      options[:file_name].to_s
+                    end
+                    
+          super Carrierwave::Base64::Base64StringIO.new(data.strip, filename)
         end
         # rubocop:enable Metrics/MethodLength, Metrics/AbcSize
         # rubocop:enable Metrics/CyclomaticComplexity
